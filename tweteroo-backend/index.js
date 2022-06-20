1cc40aaf0cba3e7f8fb0803f5adb6266c7ce2d19
Darlon Gomes <gomesdarlon@icloud.com>
2022-06-20 15:16:17 -0300

    chore: 3 of 5 bonuses completed

--- tweteroo-backend/index.js
+++ tweteroo-backend/index.js
@@ -12,6 +12,9 @@ app.post("/sign-up", (req, res) => {
 
     const userID = users.length;
     const login = {id: userID, ...req.body};
+    if(login.username === '' || login.avatar === ''){
+        return res.status(400).send("Todos os campos são obrigatórios")
+    }
     users.push(login);
     res.status(201).send("OK");
 })
@@ -19,6 +22,9 @@ app.post("/sign-up", (req, res) => {
 app.post("/tweets", (req, res) => {
     const tweetID = tweets.length;
     const tweet = {id: tweetID, username: req.headers.user, tweet: req.body.tweet}
+    if(tweet.username === '' || tweet.tweet === ''){
+        return res.status(400).send("Todos os campos são obrigatórios")
+    }
     for(let i = 0; i < users.length; i++){
         const user = users[i];
         if( user.username === tweet.username){
@@ -28,12 +34,12 @@ app.post("/tweets", (req, res) => {
     }
 
     tweets.unshift(tweet);
-    res.status(201).send("OK");
+    return res.status(201).send("OK");
 })
 
 app.get("/tweets", (req, res) => {
    
-    res.send(tweets);
+    return res.send(tweets.slice(0,10));
 })
 
 app.listen(5000);
